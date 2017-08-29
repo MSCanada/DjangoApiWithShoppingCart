b70496038e9b522173f66ce8ba47712ac47cd000
Muhammad Suhail <sohail.nedian@hotmail.com>
2017-08-28 17:30:05 -0700

    Pushing Api stuff

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url,include
+from . import views
+
+urlpatterns = [
+   
+    url(r'^tasks/$', views.task_list, name='task_list'),
+    url(r'^tasks/(?P<pk>[0-9]+)$', views.task_detail, name='task_detail'),
+
+]
